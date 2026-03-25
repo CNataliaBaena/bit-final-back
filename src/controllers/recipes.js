6c10c96ad0cd71d1c1773440311533739a4740f9
Nago1531 <baenan301@gmail.com>
2026-03-24 21:52:42 -0500

    Finishing back end

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -1,19 +1,68 @@
+import Recipe from '../models/recipes.js';
+
 const recipesController = {
-  create: (req, res) => {
-    res.send('Create a new recipe');
+  create: async (req, res) => {
+    try {
+        const { name, description, price, ingredients, instructions } = req.body;
+        const newRecipe = new Recipe({ 
+          name, 
+          description, 
+          price, 
+          ingredients, 
+          instructions 
+        });
+        await newRecipe.save();
+        res.status(201).json({ message: 'Recipe created successfully'});
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating recipe'});
+    }
   },
-  readAll: (req, res) => {
-    res.send('Read all recipes');
+  readAll: async (req, res) => {
+    try {
+      const recipes = await Recipe.find();
+      res.status(200).json({ message: 'Recipes retrieved successfully' , data: recipes });
+    } catch (error) {
+      res.status(500).json({ message: 'Error retrieving recipes'});
+    }
   },
-  read: (req, res) => {
-    res.send('Read a specific recipe');
+  read: async (req, res) => {
+    try {
+      const recipe = await Recipe.findById(req.params.id);
+      res.status(200).json({ message: 'Recipe retrieved successfully', data: recipe });
+    } catch (error) {
+      res.status(500).json({ message: 'Error retrieving recipe' });
+    }
   },
-  update: (req, res) => {
-    res.send('Update a specific recipe');
+  update: async (req, res) => {
+    try {
+      const { name, description, price, ingredients, instructions } = req.body;
+      const updatedRecipe = await Recipe.findByIdAndUpdate(
+        req.params.id,
+        { name, 
+          description, 
+          price, 
+          ingredients, 
+          instructions })
+          if (!updatedRecipe) {
+            return res.status(404).json({ message: 'Recipe not found' });
+          }else {
+            res.status(200).json({ message: 'Recipe updated successfully' });
+          }
+    } catch (error) {
+      res.status(500).json({ message: 'Error updating recipe' });
+    }
   },
-  delete: (req, res) => {
-    res.send('Delete a specific recipe');
-  }
+  delete: async (req, res) => {
+    try {
+      const deletedRecipe = await Recipe.findByIdAndDelete(req.params.id);
+      if (!deletedRecipe) {
+        return res.status(404).json({ message: 'Recipe not found' });
+      }
+      res.status(200).json({ message: 'Recipe deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error deleting recipe' });
+    }
+    }
 };
 
 export default recipesController;
---
+++ src/models/recipes.js
@@ -0,0 +1,23 @@
+import {Schema , model} from 'mongoose';
+
+const recipeSchema = new Schema({
+    name: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    ingredients: {
+        type: String
+    },
+    instructions: {
+        type: String
+    }
+}
+
+, {versionKey: false, timestamps: true });
+export default model('Recipe', recipeSchema);
+
