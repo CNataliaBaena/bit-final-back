da46e912691d6312968c5d4a49abd9381a5479fb
Nago1531 <baenan301@gmail.com>
2026-04-29 20:54:02 -0500

    Cambios para AWS

--- src/server.js
+++ src/server.js
@@ -3,7 +3,14 @@ import "dotenv/config";
 import cors from 'cors';
 import connectDB from './config/db.js';
 import dns from 'node:dns';
-import recipesRouter from './routers/recipes.js';;
+import recipesRouter from './routers/recipes.js';
+// importaciones para acceder a las rutas del front - configurar el acceso al front
+import path from "path";
+import { fileURLToPath } from "url";
+
+// configuraciones para acceder al front
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 const server = express();
@@ -21,11 +28,21 @@ server.use(express.json());
 
 server.use("/recipes", recipesRouter);
 
-server.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// server.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// vamos a hacer la petición para que se muestre nuestro front
+// Servir archivos estáticos desde la carpeta "public"
+server.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+export default server;
